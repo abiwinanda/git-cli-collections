4234a9ceecf3779b725803968d5898b3791a3a62
abiwinanda <abiwinanda@outlook.com>
2022-06-11 13:32:22 +0800

    add git clone command

---
+++ clone/git-clone.sh
@@ -0,0 +1,5 @@
+# Clone git repository by creating a folder with name the same as the repo name
+git clone ${HTTP_OR_SSH_URL}
+
+# Clone git repository by creating with custom folder name
+git clone ${HTTP_OR_SSH_URL} ${FOLDER_NAME}
