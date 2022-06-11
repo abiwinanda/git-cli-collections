c4da41f0d23d0780a4e3b3bc4ffa033ee39aae4b
abiwinanda <abiwinanda@outlook.com>
2022-06-11 13:02:30 +0800

    add more git delete branch commands

--- delete-branch.sh
+++ delete-branch.sh
@@ -1,3 +1,9 @@
+# This will fail if the branch has unmerged changes or commits
 git delete -d ${BRANCH_NAME}
 
+# This will delete the branch even if it has unmerged changes or commits
 git delete -D ${BRANCH_NAME}
+
+# Delete branch in remote. Yep it uses git push
+git push --delete ${REMOTE_NAME} ${BRANCH_NAME}
+
