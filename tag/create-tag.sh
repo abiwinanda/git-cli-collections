d5f222971af1f4d279e3c5c5b16175c92acd19b0
abiwinanda <abiwinanda@outlook.com>
2022-06-11 13:21:19 +0800

    create push tag script

--- tag/create-tag.sh
+++ tag/create-tag.sh
@@ -3,6 +3,3 @@ git tag ${TAG_NAME}
 
 # Creating a tag at a specific commit
 git tag -a ${TAG_NAME} ${COMMIT_HASH} -m ${MESSAGE}
-
-# Push tag to remote
-git tag ${REMOTE_NAME} ${TAG_NAME}
---
+++ tag/push-tag.sh
@@ -0,0 +1,5 @@
+# Push tag to remote
+git push ${REMOTE_NAME} ${TAG_NAME}
+
+# Push all newly created tags
+git push --tags
