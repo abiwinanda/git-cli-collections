58d3291b0c7bd96b2faac3661c46042cc78c5dae
abiwinanda <abiwinanda@outlook.com>
2022-06-11 13:29:07 +0800

    add more create branch command

--- branch/create-branch.sh
+++ branch/create-branch.sh
@@ -1,5 +1,11 @@
 # Creating a new branch without checkout to it
 git branch ${BRANCH_NAME}
 
+# Creating a new branch from a specific commit without checkout to it
+git branch ${BRANCH_NAME} ${COMMIT_HASH}
+
 # Creating a new branch and checkout to it
 git checkout -b ${BRANCH_NAME}
+
+# Creating a new branch from a specific commit and checkout to it
+git checkout -b ${BRANCH_NAME} ${COMMIT_HASH}
