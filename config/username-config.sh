29b9e12a5b1647678d667c34ef57d1d5201caa7d
abiwinanda <abiwinanda@outlook.com>
2022-06-18 13:23:11 +0800

    add git username config

---
+++ config/username-config.sh
@@ -0,0 +1,8 @@
+# Get git username config (repo level)
+git config --get user.name
+
+# Set git username config (repo level)
+git config user.name ${USERNAME}
+
+# Set git username config (global level)
+git config --global user.name ${USERNAME}
