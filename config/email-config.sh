76267e1d6676e4a4f8d5e07e0c406d7b9ae96496
abiwinanda <abiwinanda@outlook.com>
2022-06-18 13:23:31 +0800

    use variable

--- config/email-config.sh
+++ config/email-config.sh
@@ -2,7 +2,7 @@
 git config --get user.email
 
 # Set git email config (repo level)
-git config user.email your@email.com
+git config user.email ${EMAIL}
 
 # Set git email config (global level)
-git config --global user.email your@email.com
+git config --global user.email ${EMAIL}
