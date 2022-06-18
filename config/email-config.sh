90a336bb3ba7a3a17ae4d76c896a8584201f5c8c
abiwinanda <abiwinanda@outlook.com>
2022-06-18 13:21:49 +0800

    add git email config

---
+++ config/email-config.sh
@@ -0,0 +1,8 @@
+# Get git email config (repo level)
+git config --get user.email
+
+# Set git email config (repo level)
+git config user.email your@email.com
+
+# Set git email config (global level)
+git config --global user.email your@email.com
