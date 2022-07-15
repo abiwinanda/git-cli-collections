a679b4384b1ff1895a067f7354e17dd26e01b661
abiwinanda <abiwinanda@outlook.com>
2022-07-15 09:05:40 +0800

    add git clone command with specific ssh key

--- clone/git-clone.sh
+++ clone/git-clone.sh
@@ -3,3 +3,6 @@ git clone ${HTTP_OR_SSH_URL}
 
 # Clone git repository in a current DIR with a custom folder name
 git clone ${HTTP_OR_SSH_URL} ${FOLDER_NAME}
+
+# Clone git repository with a specific SSH key
+GIT_SSH_COMMAND="ssh -i path/to/ssh/secret/key" git clone ${HTTP_OR_SSH_URL}
